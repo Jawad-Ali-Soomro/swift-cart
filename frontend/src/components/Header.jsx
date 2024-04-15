c408fbbc8b30b726326e910c62880bafd5497952
unknown <jawadal829@gmail.com>
2024-04-15 01:15:41 -0700

    updated ui

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -7,6 +7,9 @@ import {
   BiSupport,
   BiSearch,
   BiCategory,
+  BiEnvelope,
+  BiMessage,
+  BiArrowToBottom,
 } from "react-icons/bi";
 
 const Header = () => {
@@ -19,15 +22,31 @@ const Header = () => {
         <BiCategory className="icon" />
         <BiUser className="icon" />
       </div>
-      <div className="help flex"  onClick={() => set_show_help(!help)}>
+      <div className="help flex" onClick={() => set_show_help(true)}>
         <BiSupport />
         <div
-          className="help-main-wrap"
+          className="help-main-wrap flex col"
           style={{
             maxHeight: `${help == true ? "400px" : "0%"}`,
             border: `${help == true ? "1px solid rgba(0,0,0,0.1)" : "none"}`,
           }}
-        ></div>
+        >
+          <div className="input-wrap flex">
+            <BiEnvelope className="icon" />
+            <input type="text" placeholder="E M A I L " />
+          </div>
+          <div className="input-wrap flex">
+            <BiUser className="icon" />
+            <input type="text" placeholder="N A M E" />
+          </div>
+          <div className="message-wrap flex">
+            <textarea name="" id="" cols="30" rows="10" placeholder="M E S S A G E"></textarea>
+          </div>
+          <button>Send</button>
+          <div className="close-btn flex" >
+            <BiArrowToBottom onClick={() => set_show_help(false)} />
+          </div>
+        </div>
       </div>
     </div>
   );
